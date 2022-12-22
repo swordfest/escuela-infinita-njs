d1c9de3f586ae35b365f80fa907eef70f2cc864b
swordfest <alexanderhavarro@gmail.com>
2022-12-22 00:02:01 -0500

    styling

--- components/header.tsx
+++ components/header.tsx
@@ -1,11 +1,14 @@
+import Image from "next/image";
+
 export default function Header() {
 	return (
 		<header className="header relative w-screen left-1/2 right-1/2 ml-[-50vw] mr-[-50vw] h-auto col-span-12 flex items-center justify-center">
-			<img
+			{/* <img
 				className=" object-cover w-full h-[1084px]"
 				src={"https://images.unsplash.com/photo-1508169351866-777fc0047ac5"}
 				alt="book opened"
-			/>
+			/> */}
+            <Image placeholder="blur" blurDataURL="/imgs/header.jpg" fill style={{objectFit:"cover", width: '100%', height: '100%'}} src={"/imgs/header.jpg"} alt={"header"}/>
 		</header>
 	);
 }
