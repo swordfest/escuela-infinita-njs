47b4cdc29b0dff9e2a4eb51804014f2cc1e6e53d
Alex Navarro <alexanderhavarro@gmail.com>
2023-01-04 14:10:20 -0800

    styling

--- components/button.tsx
+++ components/button.tsx
@@ -33,7 +33,7 @@ export default function Button(props: any) {
                     <svg className={("fill-[#0d2636] transition-transform ") + (open && 'rotate-180')} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M16.293 9.293 12 13.586 7.707 9.293l-1.414 1.414L12 16.414l5.707-5.707z"></path></svg>
                 </span>
 
-                <ul className={("menu absolute w-full h-auto top-14 bg-[#174563] text-[#0d2636] font-semibold flex flex-col items-center justify-start transition-transform  ") + (open ? '' : 'hidden')}>
+                <ul className={("menu absolute w-full h-auto top-14 bg-[#98CCA5] text-[#0d2636] font-semibold flex flex-col items-center justify-start transition-transform  ") + (open ? '' : 'hidden')}>
                     <li className=" w-full h-14 flex items-center justify-start px-4 bg-[#98CCA5] transition-all hover:bg-[#85b390]">Capitulo 1</li>                    
                     <li className=" w-full h-14 flex items-center justify-start px-4 bg-[#98CCA5] transition-all hover:bg-[#85b390]">Capitulo 2</li>                    
                     <li className=" w-full h-14 flex items-center justify-start px-4 bg-[#98CCA5] transition-all hover:bg-[#85b390]">Capitulo 3</li>                    
@@ -44,7 +44,7 @@ export default function Button(props: any) {
     }
     if (props.type === 'slider') {
         return (
-            <button title="slide-button" name="Descarga" className={(" absolute z-[1] w-10 h-10 bg-[#174563] border-red-100 hover:scale-105 select-none flex items-center justify-center transition-all hover:bg-[#143c57] active:bg-[#194c6e] ") + (props.side === 'left' ? '-left-12 top-[40%] ' : '') + (props.side === 'right' ? '-right-12 top-[40%] ' : '')} >
+            <button title="slide-button" name="Descarga" className={(" absolute z-[1] w-10 h-10 bg-[#174563] hover:scale-105 select-none flex items-center justify-center transition-all hover:bg-[#143c57] active:bg-[#194c6e] ") + (props.side === 'left' ? '-left-12 top-[40%] ' : '') + (props.side === 'right' ? '-right-12 top-[40%] ' : '')} >
                 <svg className={props.side === 'left' ? 'fill-white' : 'hidden'} width="36" height="36" viewBox="0 0 24 24"><path d="M13.293 6.293 7.586 12l5.707 5.707 1.414-1.414L10.414 12l4.293-4.293z"></path></svg>
                 <svg className={props.side === 'right' ? 'fill-white' : 'hidden'} width="36" height="36" viewBox="0 0 24 24"><path d="M10.707 17.707 16.414 12l-5.707-5.707-1.414 1.414L13.586 12l-4.293 4.293z"></path></svg>
             </button>
--- components/header.tsx
+++ components/header.tsx
@@ -1,19 +1,25 @@
 import Image from "next/image";
-import Rive, { Alignment, Layout, useRive } from "@rive-app/react-canvas";
+import Rive, { Alignment, Fit, Layout, useRive } from "@rive-app/react-canvas";
 import Button from "./button";
 import { useEffect, useState } from "react";
 // import riveWasmUrl from '@rive-app/canvas/rive.wasm';
 // import { RuntimeLoader } from 'rive-react';
 
-export default function Header(props:any) {
+export default function Header(props: any) {
 	const { rive, RiveComponent } = useRive({
 		src: '/animations/escuela_infinita_artboard_small.riv',
 		autoplay: true,
+		artboard: 'PortadaSmall',
 		layout: new Layout({
+			fit: Fit.FitHeight,
 			alignment: Alignment.TopLeft,
-			// maxX: 480,
-		})
-	  });
+			maxX: 480,
+		}),
+		
+	}, {
+		useDevicePixelRatio: true,
+		// fitCanvasToArtboardHeight: true,
+	});
 
 	const [enter, setEnter] = useState(true)
 
@@ -22,42 +28,44 @@ export default function Header(props:any) {
 	// 	setEnter(true)
 	// }
 	// })
-	
+
 	return (
 		//  left-1/2 right-1/2 ml-[-50vw] mr-[-50vw]
-		<header className="header w-full h-[640px] xl:h-[860px] bg-gradient-to-tr from-[#162330] to-[#1b2b3b] col-span-12 flex items-center justify-start mt-20 ">
+		<header className="header relative w-full h-[640px] xl:h-[860px] bg-gradient-to-tr from-[#162330] to-[#23374b] col-span-12 flex items-center justify-start mt-20 ">
 			{/* <img className="w-full h-full object-cover " src="/imgs/header.jpg" alt="header" /> */}
 			{/* <Rive style={{display: 'flex', alignItems: 'start', }} src="/animations/escuela_infinita_artboard_small.riv" /> */}
-			<RiveComponent/>
-			<div className="w-full h-full text-white flex flex-col items-end justify-center gap-2 pr-32">
-				<span className="font-black text-7xl text-end ">LA ESCUELA INFINITA</span>
-				<span className="font-bold text-7xl text-end ">Aprender y enseñar en entornos ubicuos</span>
-				<div className={("container h-auto xl:h-40 mx-auto flex flex-col lg:flex-row items-center justify-end gap-6 py-4 ")}>
-				<Button
-					key={'but'}
-					text={"DESCARGAR LIBRO - PDF"}
-					type={"button"}
-					link={"#"}
-                    isVisible={enter}
-                    delay={'100'}
-				/>
-				<Button
-					key={'but2'}
-					text={"DESCARGAR LIBRO - EPUB"}
-					type={"button"}
-					link={"#"}
-                    isVisible={enter}
-                    delay={'300'}
-				/>
-				<Button
-					key={'but3'}
-					text={"DESCARGAR CAPITULOS"}
-					type={"menu"}
-					link={"#"}
-                    isVisible={enter}
-                    delay={'500'}
-				/>
-			</div>
+			<RiveComponent />
+			<div className=" absolute w-full h-full top-0 right-0 text-white">
+				<div className="container mx-auto px-4 h-full flex flex-col items-end justify-center">
+					<span className="font-black text-5xl lg:text-7xl text-end ">LA ESCUELA INFINITA</span>
+					<span className="font-bold text-2xl lg:text-5xl text-end ">Aprender y enseñar en entornos ubicuos</span>
+					<div className={("container h-auto xl:h-40 mx-auto flex flex-col lg:flex-row items-center justify-end gap-6 py-4 ")}>
+						<Button
+							key={'but'}
+							text={"DESCARGAR LIBRO - PDF"}
+							type={"button"}
+							link={"#"}
+							isVisible={enter}
+							delay={'100'}
+						/>
+						<Button
+							key={'but2'}
+							text={"DESCARGAR LIBRO - EPUB"}
+							type={"button"}
+							link={"#"}
+							isVisible={enter}
+							delay={'300'}
+						/>
+						<Button
+							key={'but3'}
+							text={"DESCARGAR CAPITULOS"}
+							type={"menu"}
+							link={"#"}
+							isVisible={enter}
+							delay={'500'}
+						/>
+					</div>
+				</div>
 			</div>
 		</header>
 	);
