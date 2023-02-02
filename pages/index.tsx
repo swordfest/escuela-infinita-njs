2dbdf7621e4fb9382ae5f56e7fa3aa979666e64b
swordfest <alexanderhavarro@gmail.com>
2023-02-02 01:07:03 -0500

    trying a fix for the scroll

--- pages/index.tsx
+++ pages/index.tsx
@@ -77,6 +77,7 @@ export default function Home(props: any) {
 	};
 
 	useEffect(() => {
+		window.addEventListener('onload', () => window.scrollTo(0, 0))
 		setReviews(props.reviews);
 		setCursos(props.cursos);
 		console.log(scrollPos);
